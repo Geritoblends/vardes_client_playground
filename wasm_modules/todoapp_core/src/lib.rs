341cc68a7d895bd30ce4c9ef921eb0d0329eb7af
gero <geronimosotelolopez@gmail.com>
2025-09-20 23:59:10 -0600

    singletons possible in wasm modules

--- wasm_modules/todoapp_core/src/lib.rs
+++ wasm_modules/todoapp_core/src/lib.rs
@@ -1,6 +1,8 @@
-struct TodoCore {
+pub struct TodoCore {
     db: sled::Db
 }
+
+static TODO_CORE: Lazy<Mutex<Option<TodoCore>>> = Lazy::new(|| Mutex::new(None));
 
 #[repr(C)]
 pub struct TaskMessage {
@@ -13,7 +15,7 @@ pub struct TaskMessage {
 
 extern "C" {
     fn lookup_func_index(ptr: i64, len: i64) -> i64;
-    fn get_consumer_fn_indices(ptr: *const str, len: i64) -> (*const i32, len: i32);
+    fn get_consumer_fn_indices(ptr: *const str, len: i64) -> (*const i64, len: i64);
 }
 
 #[no_mangle]
@@ -22,7 +24,11 @@ pub extern "C" fn allocate(len: i32) -> i32 {
 
 #[no_mangle]
 pub extern "C" fn init() {
-
+    let db = sled::Db::new();
+    let core = TodoCore { db };
 }
 
-
+#[no_mangle]
+pub extern "C" fn add_item(ptr: *mut TodoCore, item_ptr: *const Task) -> i32 {
+    unimplemented!();
+}
